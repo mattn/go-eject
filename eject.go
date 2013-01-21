fb68c6ad213424f5a46eb4bd1e726b80caefec55
mattn <mattn.jp@gmail.com>
2013-01-21 16:23:09 +0900

    first import.

---
+++ eject.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/mattn/go-ole"
+	"github.com/mattn/go-ole/oleutil"
+)
+
+func Eject() {
+	ole.CoInitialize(0)
+
+	unk, _ := oleutil.CreateObject("WMPlayer.OCX")
+	wmp, _ := unk.QueryInterface(ole.IID_IDispatch)
+	drives := oleutil.MustGetProperty(wmp, "cdromCollection").ToIDispatch()
+	for i := 0; i < int(oleutil.MustGetProperty(drives, "Count").Val); i++ {
+		drive := oleutil.MustCallMethod(drives, "Item", i).ToIDispatch()
+		oleutil.MustCallMethod(drive, "Eject")
+	}
+}
+
+func main() {
+	Eject()
+}
+
